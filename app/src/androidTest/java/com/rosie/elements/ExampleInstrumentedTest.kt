79b2adc73720fd542cea32aa23cee0cdf2a561bb
Rosie Hamilton <enchantrix@gmail.com>
2018-05-09 20:35:42 +0100

    to do assert some text

--- app/src/androidTest/java/com/rosie/elements/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/rosie/elements/ExampleInstrumentedTest.kt
@@ -1,7 +1,16 @@
 package com.rosie.elements
 
+import android.app.Activity
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import android.util.Log
+import android.widget.TextView
+import com.rosie.elements.R.id.hello_text
+//import com.rosie.elements.R.id.main_text
+import com.rosie.elements.R.id.text
+import org.junit.Assert
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,4 +30,21 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.rosie.elements", appContext.packageName)
     }
+
+    //@Test
+    //fun test_itDisplaysHelloWorld()
+       // import the view, not the id
+
+       // val text = findViewById<TextView>(R.id.main_text)
+       // val textView : textView  = main_text
+       // val textView: TextView = findViewById<TextView>(R.id.main_text) as TextView
+
+       // Log.d("TAG", textView)
+       // val actual = textView.text.toString()
+       // Assert.assertEquals("Hello World!", actual)
+    //}
 }
+
+
+
+
