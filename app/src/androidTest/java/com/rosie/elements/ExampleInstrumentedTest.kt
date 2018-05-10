67e87c96cc96c06d277f1b3434e6a3a5e090ccfc
Rosie Hamilton <enchantrix@gmail.com>
2018-05-10 15:33:16 +0100

    ask for location permission

--- app/src/androidTest/java/com/rosie/elements/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/rosie/elements/ExampleInstrumentedTest.kt
@@ -1,20 +1,9 @@
 package com.rosie.elements
 
-import android.app.Activity
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import android.util.Log
-import android.widget.TextView
-import com.rosie.elements.R.id.hello_text
-//import com.rosie.elements.R.id.main_text
-import com.rosie.elements.R.id.text
-import org.junit.Assert
-
-import kotlinx.android.synthetic.main.activity_main.*
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 /**
@@ -30,19 +19,6 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.rosie.elements", appContext.packageName)
     }
-
-    //@Test
-    //fun test_itDisplaysHelloWorld()
-       // import the view, not the id
-
-       // val text = findViewById<TextView>(R.id.main_text)
-       // val textView : textView  = main_text
-       // val textView: TextView = findViewById<TextView>(R.id.main_text) as TextView
-
-       // Log.d("TAG", textView)
-       // val actual = textView.text.toString()
-       // Assert.assertEquals("Hello World!", actual)
-    //}
 }
 
 
--- app/src/main/java/com/rosie/elements/MainActivity.kt
+++ app/src/main/java/com/rosie/elements/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.rosie.elements
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+
+val MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0
+val MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // check if have permission to access location
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        && (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED))
+        {
+            Log.d("TAG", "location permission not granted")
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+                // show explanation and try again to request permission
+            } else {
+                //no explanation needed can request the permission
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION)
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+                // MY_PERMISSIONS_REQUESTS are app defined constants the callback gets the result of the request
+            }
+
+        } else {
+            Log.d("TAG", "location permission is granted")
+        }
+
+
+
+
+
     }
 }
