80879276cfe2f9854864032475e76e780bf43bcd
Rosie Hamilton <enchantrix@gmail.com>
2018-05-14 13:31:55 +0100

    refactor and clean up

--- app/src/androidTest/java/com/rosie/elements/MainActivityTest.kt
+++ app/src/androidTest/java/com/rosie/elements/MainActivityTest.kt
@@ -1,12 +1,13 @@
 package com.rosie.elements
 
-import android.support.test.espresso.Espresso
-import android.support.test.espresso.ViewAssertion
+import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions
-import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
+import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -15,21 +16,29 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @Rule @JvmField
+
+    // a rule to launch a single activity prior to every test
+    @Rule
+    @JvmField
     val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    // a rule to grant tests the run time permission ACCESS_FINE_LOCATION
+    @Rule
+    @JvmField
+    val permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
 
     @Test
     fun shouldSayHello() {
-        Espresso.onView(ViewMatchers.withId(R.id.welcome_text))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-                .check(ViewAssertions.matches(withText("Hello")))
+        onView(withId(R.id.welcome_text))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("Hello")))
     }
 
     @Test
     fun shouldDisplayGettingLocationTextAfterGetLocationButtonClicked(){
-        Espresso.onView(ViewMatchers.withId(R.id.location_button)).perform(click())
-        Espresso.onView(ViewMatchers.withId(R.id.location_text_view))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.location_button)).perform(click())
+        onView(withId(R.id.location_text_view))
+                .check(matches(isDisplayed()))
 
     }
 }
--- app/src/main/java/com/rosie/elements/MainActivity.kt
+++ app/src/main/java/com/rosie/elements/MainActivity.kt
@@ -56,18 +56,16 @@ class MainActivity : AppCompatActivity()  {
 
     fun askForLocationPermission(){
         // check if have permission to access location
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED))
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             Log.d("TAG", "location permission not granted")
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                    && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
             {
                 // show explanation and try again to request permission
             } else {
                 //no explanation needed can request the permission
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), MY_LOCATION_PERMISSIONS)
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_LOCATION_PERMISSIONS)
 
                 // MY_PERMISSIONS_REQUESTS are app defined constants the callback gets the result of the request
             }
